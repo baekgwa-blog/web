71bc93c123b31ec008e4104301b5e41091629476
Baegwa <160799011+KangBaekGwa@users.noreply.github.com>
2025-05-12 11:30:01 +0900

    [🌟feat] : 메인페이지 더보기 (무한 스크롤) 적용 (#9)
    * feat : 무한 스크롤 적용
    - react-query 적용
    - intersection observer 적용
    
    * style : 메인페이지 프로필 섹션 소개 페이지로 이동
    
    * fix : 안쓰는 component import 삭제

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,10 @@
-import ProfileSection from '@/app/_components/ProfileSection';
-import ContactSection from '@/app/_components/ContactSection';
+import { getTags, getPublishedPosts } from '@/lib/notion';
 import HeaderSection from '@/app/_components/HeaderSection';
-import { getTags } from '@/lib/notion';
 import PostListSuspense from '@/components/features/blog/PostListSuspense';
 import { Suspense } from 'react';
 import TagSectionClient from '@/app/_components/TagSection.client';
-import TagSectionSkeleton from './_components/TagSectionSkeleton';
 import PostListSkeleton from '@/components/features/blog/PostListSkeleton';
-
+import TagSectionSkeleton from '@/app/_components/TagSectionSkeleton';
 interface HomeProps {
   searchParams: Promise<{ tag?: string; sort?: string }>;
 }
@@ -18,10 +15,10 @@ export default async function Home({ searchParams }: HomeProps) {
   const selectedSort = sort || 'latest';
 
   const tags = getTags();
-
+  const postsPromise = getPublishedPosts({ tag: selectedTag, sort: selectedSort });
   return (
     <div className="container py-8">
-      <div className="grid grid-cols-[200px_1fr_220px] gap-6">
+      <div className="grid grid-cols-[200px_1fr] gap-6">
         {/* 좌측 사이드바 */}
         <aside>
           <Suspense fallback={<TagSectionSkeleton />}>
@@ -31,18 +28,16 @@ export default async function Home({ searchParams }: HomeProps) {
         <div className="space-y-8">
           {/* 섹션 제목 */}
           <HeaderSection selectedTag={selectedTag} />
-
           {/* 블로그 카드 그리드 */}
-          {/* 첫번째, 이미지만 priority 처리를 위해, 인덱스 전달 */}
           <Suspense fallback={<PostListSkeleton />}>
-            <PostListSuspense selectedTag={selectedTag} selectedSort={selectedSort} />
+            <PostListSuspense postsPromise={postsPromise} />
           </Suspense>
         </div>
         {/* 우측 사이드바 */}
-        <aside className="flex flex-col gap-6">
+        {/* <aside className="flex flex-col gap-6">
           <ProfileSection />
           <ContactSection />
-        </aside>
+        </aside> */}
       </div>
     </div>
   );
