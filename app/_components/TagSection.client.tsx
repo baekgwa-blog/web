be6b4870ea5b04b332fba8f9c3240a2183f56e3b
KangBaekGwa <ksu9801@gmail.com>
2025-06-10 16:40:43 +0900

    fix : 배포 환경에서 오류 수정

--- app/_components/TagSection.client.tsx
+++ app/_components/TagSection.client.tsx
@@ -5,7 +5,6 @@ import Link from 'next/link';
 import { TagFilterItem } from '@/types/blog';
 import { cn } from '@/lib/utils';
 import { use } from 'react';
-import TagSectionSkeleton from './TagSectionSkeleton';
 
 interface TagSectionProps {
   tags: Promise<TagFilterItem[]>;
@@ -13,38 +12,16 @@ interface TagSectionProps {
 }
 
 export default function TagSection({ tags, selectedTag }: TagSectionProps) {
-  let allTags: TagFilterItem[] = [];
-  let error: Error | null = null;
-
-  try {
-    allTags = use(tags);
-  } catch (e) {
-    error = e as Error;
-  }
-
-  if (error) {
-    return <TagSectionSkeleton />;
-  }
-
+  const allTags = use(tags);
   return (
     <Card>
       <CardHeader>
         <CardTitle>태그 목록</CardTitle>
       </CardHeader>
       <CardContent>
-        <div className="space-y-4">
-          <Link href="/" className="block">
-            <div
-              className={cn(
-                'hover:bg-muted-foreground/10 text-muted-foreground flex items-center justify-between rounded-md p-1.5 text-sm transition-colors',
-                selectedTag === '전체' && 'bg-muted-foreground/10 text-foreground font-medium'
-              )}
-            >
-              <span>전체</span>
-            </div>
-          </Link>
+        <div className="flex flex-col gap-3">
           {allTags.map((tag) => (
-            <Link href={`?tag=${tag.name}`} key={tag.name} className="block">
+            <Link href={`?tag=${tag.name}`} key={tag.name}>
               <div
                 className={cn(
                   'hover:bg-muted-foreground/10 text-muted-foreground flex items-center justify-between rounded-md p-1.5 text-sm transition-colors',
