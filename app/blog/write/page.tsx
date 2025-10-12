2f26afc3616b40e516b2d74cb5476618d0d2d2b3
KangBaekGwa <ksu9801@gmail.com>
2025-10-13 00:25:33 +0900

    feat: Add metadata generation for WritePage component - Implement generateMetadata function to set the page title for the blog writing interface - Enhance SEO and user experience with dynamic title rendering

--- app/blog/write/page.tsx
+++ app/blog/write/page.tsx
@@ -1,5 +1,9 @@
 import WritePostForm from '@/components/features/blog/write/WritePostForm';
 
+export async function generateMetadata() {
+  return { title: `글 작성` };
+}
+
 export default function WritePage() {
   return (
     <div className="container flex h-[calc(100vh-var(--header-height))] flex-col px-4 py-4">
